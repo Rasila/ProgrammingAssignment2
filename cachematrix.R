0e45bb2650873c4bd09e7c7aa0a68fb52ec61c5f
Rasila <34964041+Rasila@users.noreply.github.com>
2018-01-02 17:25:46 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The combination of functions below compute the inverse of an invertible matrix,
+# but they only actually do the computations if the functions haven't been applied to this particular
+# matrix before.
 
-## Write a short comment describing this function
+# The first part, makeCacheMatrix, takes a matrix and gives back a list of 4 functions: 
+# 1) set: puts the input matrix in the cache 2) get: returns the input matrix 
+# 3) setinverse: puts the inverse, once computed, in the cache 4) getinverse: returns the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y){
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set=set,get=get,setinverse = setinverse,getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+## The second part, cacheSolve, first checks if the inverse of the matrix has been computed 
+# before (that is, if cacheSolve has been applied to this matrix before). If so, it takes
+# the inverse from the cache. Otherwise, it computes the inverse with the regular 'solve' function. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)){
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
+
+# This is how I tested if my functions worked.
+A <- matrix(1:4,nrow=2,ncol=2)
+A2 <- makeCacheMatrix(A)
+cacheSolve(A2)
+cacheSolve(A2)
